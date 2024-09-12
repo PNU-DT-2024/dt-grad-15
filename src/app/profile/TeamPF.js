433dcc3f2eced68a39b42acb382f7b1cf6ffb276
jeonghye park <byulnim2002@gmail.com>
2024-09-12 21:26:27 +0900

    feat 팀 프로필/프로젝트페이지 css 추가

--- src/app/profile/TeamPF.js
+++ src/app/profile/TeamPF.js
@@ -1,4 +1,5 @@
 import Menu from "../common/Menu";
+import React, { useState } from 'react';
 // import { useParams } from "react-router-dom";
 import SubHeading from "../common/SubHeading";
 import Footer from "../common/Footer";
@@ -7,6 +8,10 @@ import data from "../../components/team.json";
 import styles from "../../css/TeamPF.module.css";
 function TeamPF() {
     // let { id } = useParams();
+    const [hover, setHover] = useState('WEB');
+    function onClickTeam(team) {
+        setHover(team);
+    }
     return (
         <div>
             <Menu />
@@ -15,37 +20,37 @@ function TeamPF() {
                 <section className={`${styles.teamList} row`}>
                     {/* <hr /> */}
                     {data.team.map((item) => (
-                        <article className={`${styles.teamCardOpen} ${styles.teamCardClose} coloumn`}>
-                            {/* <div className={styles.columnName}>
-                                <span>{item.name}</span>
-                            </div> */}
-                            <div>
-                            <div className={styles.teamTitle}>
-                                <h1>{item.name}</h1>
-                                <p className="description">{item.role}</p>
-                            </div>
-                            <div className={styles.teamMember}>
-                                <p>구성원</p>
-                                <div className={`${styles.memberContainer} row`}>
-                                    {item.member.map((member) => (
-                                        <div>
-                                            <Link to={`/profile/indiv/${member}`}>
-                                                <div className={styles.memberImg}>
-                                                    <img src="#" alt="#" />
-                                                </div>
-                                                <div>{member}</div>
+                        <article onClick={() => { onClickTeam(item.name) }} className={`${hover === item.name ? styles.teamCardOpen : styles.teamCardClose} coloumn`}>
+                            {hover === item.name ? <div>
+                                <div className={styles.teamTitle}>
+                                    <h1>{item.name}</h1>
+                                    <p className="description">{item.role}</p>
+                                </div>
+                                <div className={styles.teamMember}>
+                                    <p>구성원</p>
+                                    <div className={`${styles.memberContainer} row`}>
+                                        {item.member.map((member) => (
+                                            <div>
+                                                <Link to={`/profile/indiv/${member}`}>
+                                                    <div className={styles.memberImg}>
+                                                        <img src="#" alt="#" />
+                                                    </div>
+                                                    <div>{member}</div>
 
-                                            </Link>
-                                        </div>
-                                    ))}
+                                                </Link>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                            <div className={styles.moveLink}>
                                 <Link to={`/project/team/${item.name}`}>
-                                    <span>팀 프로젝트 보러가기</span>
+                                    <div className={styles.moveLink}>
+                                        <span>팀 프로젝트 보러가기</span>
+                                    </div>
                                 </Link>
-                                </div>
-                            </div>
+                            </div> : <div className={styles.closeTeam}>
+                                <span>{item.name}</span>
+                            </div>}
+
                         </article>
 
                     ))}
--- src/app/project/TeamPJ.js
+++ src/app/project/TeamPJ.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 import Menu from "../common/Menu";
 import SubHeading from "../common/SubHeading";
 import Footer from "../common/Footer";
@@ -6,30 +7,40 @@ import data from "../../components/team.json";
 import styles from "../../css/TeamPJ.module.css";
 import videoImg from "../../components/img/video/videoImg.png";
 function TeamPJ() {
+    const [hover, setHover] = useState('WEB');
+    function onClickTeam(team) {
+        setHover(team);
+    }
     return (
         <div>
             <Menu />
             <main className="contentsContainer">
-                <SubHeading title={"팀 프로젝트"}/>
+                <SubHeading title={"팀 프로젝트"} />
                 <section className='column'>
-                    <hr className={styles.horizontalLine}/>
+                    {/* <hr className={styles.horizontalLine}/> */}
                     {data.team.map((item) => (
-                        <article>
-                            <div className={`${styles.teamCard} row`}>
-                                <div className={styles.teamTitle}>
-                                    <p>{item.name}</p>
-                                    <h1>{item.mainTitle}</h1>
-                                    <div className={styles.moveLink}>
+                        <article onClick={() => { onClickTeam(item.name) }} className={hover === item.name ? styles.teamCardOpen : styles.teamCardClose}>
+                            {hover === item.name ?
+                                <div className="row">
+                                    <div className={styles.teamTitle}>
+                                        <p>{item.name}</p>
+                                        <h1>{item.mainTitle}</h1>
                                         <Link to={`/project/team/${item.name}`}>
-                                            <span>바로가기</span>
+                                            <div className={styles.moveLink}>
+                                                <span>바로가기</span>
+                                            </div>
                                         </Link>
                                     </div>
-                                </div>
-                                <div className={styles.pjImg}>
-                                    <img src={videoImg} alt="프로젝트 이미지" />
-                                </div>
-                            </div>
-                            <hr className={styles.horizontalLine}/>
+                                    <div className={styles.pjImg}>
+                                        <img src={videoImg} alt="프로젝트 이미지" />
+                                    </div>
+                                </div> :
+                                <div className={styles.closeTeam}>
+                                    <span>{item.name}</span>
+                                </div>}
+
+
+
                         </article>
 
                     ))}
