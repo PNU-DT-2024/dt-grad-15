a4190f984f7b711d372b4ef526f89c89006f9be5
jeonghye park <byulnim2002@gmail.com>
2024-09-22 19:57:30 +0900

    add confettil 추가

--- src/app/guestbook/MessageTxt.js
+++ src/app/guestbook/MessageTxt.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import Sparkle from "./Sparkle";
+import Confetti from "./Confetti";
 import styles from "../../css/MessageTxt.module.css";
 export default function MessageTxt() {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   return (
-    <section className={styles.interTxt}>
+    <section className={styles.interTxt} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {isHovered === true ? <Confetti /> : ""}
+
       <p>학생들에게 응원의 메시지를 전해주세요!</p>
     </section>
   );
